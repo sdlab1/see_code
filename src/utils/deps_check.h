3c550666524a5f68b7ff85b4638ee271e5a49744
sdlab1 <59804621+sdlab1@users.noreply.github.com>
2025-09-04 21:52:56 +0300

    Update deps_check.h

--- src/utils/deps_check.h
+++ src/utils/deps_check.h
@@ -1,6 +1,6 @@
+// src/utils/deps_check.h
 #ifndef SEE_CODE_DEPS_CHECK_H
 #define SEE_CODE_DEPS_CHECK_H
-
 // Dependency checking functions
 int deps_check_all(void);
 int deps_check_termux_environment(void);
@@ -8,15 +8,13 @@ int deps_check_termux_gui_app(void);
 int deps_check_libraries(void);
 int deps_check_fonts(void);
 int deps_check_permissions(void);
-
 // Individual dependency checks
 int deps_check_gles2(void);
 int deps_check_freetype(void);
 int deps_check_cjson(void);
 int deps_check_pthread(void);
-
+int deps_check_termux_gui_c(void); // NEW
 // Utility functions
 void deps_print_report(void);
 const char* deps_get_missing_solution(const char* component);
-
 #endif // SEE_CODE_DEPS_CHECK_H
