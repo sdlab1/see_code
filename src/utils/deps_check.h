3f200a528a1fc2d8b1b0f1c334d7c0f3a57912ff
sdlab1 <59804621+sdlab1@users.noreply.github.com>
2025-09-04 18:44:42 +0300

    Create deps_check.h

---
+++ src/utils/deps_check.h
@@ -0,0 +1,22 @@
+#ifndef SEE_CODE_DEPS_CHECK_H
+#define SEE_CODE_DEPS_CHECK_H
+
+// Dependency checking functions
+int deps_check_all(void);
+int deps_check_termux_environment(void);
+int deps_check_termux_gui_app(void);
+int deps_check_libraries(void);
+int deps_check_fonts(void);
+int deps_check_permissions(void);
+
+// Individual dependency checks
+int deps_check_gles2(void);
+int deps_check_freetype(void);
+int deps_check_cjson(void);
+int deps_check_pthread(void);
+
+// Utility functions
+void deps_print_report(void);
+const char* deps_get_missing_solution(const char* component);
+
+#endif // SEE_CODE_DEPS_CHECK_H
