459664a5b7f64075addc66a2769870c0d3dc1570
sdlab1 <59804621+sdlab1@users.noreply.github.com>
2025-09-04 18:53:24 +0300

    Create renderer.h

---
+++ src/gui/renderer.h
@@ -0,0 +1,22 @@
+#ifndef SEE_CODE_RENDERER_H
+#define SEE_CODE_RENDERER_H
+
+#include <EGL/egl.h>
+#include <GLES2/gl2.h>
+
+// Forward declaration
+typedef struct Renderer Renderer;
+
+// Renderer functions
+Renderer* renderer_create(int width, int height);
+void renderer_destroy(Renderer* renderer);
+int renderer_begin_frame(Renderer* renderer);
+int renderer_end_frame(Renderer* renderer);
+void renderer_resize(Renderer* renderer, int width, int height);
+
+// Rendering functions
+void renderer_clear(float r, float g, float b, float a);
+void renderer_draw_quad(float x, float y, float width, float height, 
+                       float r, float g, float b, float a);
+
+#endif // SEE_CODE_RENDERER_H
