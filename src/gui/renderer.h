7a7791a9b14b7080feab22a14babfada342ac85f
sdlab1 <59804621+sdlab1@users.noreply.github.com>
2025-09-05 03:50:50 +0300

    Update renderer.h

--- src/gui/renderer.h
+++ src/gui/renderer.h
@@ -4,6 +4,10 @@
 
 #include <EGL/egl.h>
 #include <GLES2/gl2.h>
+// --- Добавлено для FreeType ---
+#include <ft2build.h>
+#include FT_FREETYPE_H
+// --- Конец добавления ---
 
 // Forward declaration
 typedef struct Renderer Renderer;
@@ -19,8 +23,13 @@ void renderer_resize(Renderer* renderer, int width, int height);
 void renderer_clear(float r, float g, float b, float a);
 void renderer_draw_quad(float x, float y, float width, float height,
                        float r, float g, float b, float a);
-// --- ФУНКЦИЯ РЕНДЕРИНГА ТЕКСТА ---
+// --- ОБНОВЛЕННАЯ ФУНКЦИЯ РЕНДЕРИНГА ТЕКСТА ---
 void renderer_draw_text(Renderer* renderer, const char* text, float x, float y, float scale, unsigned int color);
-// --- КОНЕЦ ФУНКЦИИ РЕНДЕРИНГА ТЕКСТА ---
+// --- КОНЕЦ ОБНОВЛЕННОЙ ФУНКЦИИ РЕНДЕРИНГА ТЕКСТА ---
+
+// --- Добавлено для FreeType ---
+// Инициализирует FreeType и загружает шрифт
+int renderer_init_freetype(Renderer* renderer, const char* font_path);
+// --- Конец добавления ---
 
 #endif // SEE_CODE_RENDERER_H
