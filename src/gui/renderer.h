4b634d04f96d0eead34f9355de1fa7678da76ad2
sdlab1 <59804621+sdlab1@users.noreply.github.com>
2025-09-06 08:55:41 +0300

    Update renderer.h

--- src/gui/renderer.h
+++ src/gui/renderer.h
@@ -1,6 +1,4 @@
 // src/gui/renderer.h
-// ​Интерфейс рендерера изменился,
-// чтобы поддерживать батчинг.
 #ifndef SEE_CODE_RENDERER_H
 #define SEE_CODE_RENDERER_H
 
@@ -16,22 +14,20 @@ void renderer_destroy(Renderer* renderer);
 void renderer_resize(Renderer* renderer, int width, int height);
 
 // --- Функции рендеринга кадра ---
-// Начинает запись команд в батч
 void renderer_begin_frame(Renderer* renderer);
-// Отправляет все накопленные команды на отрисовку
 void renderer_flush(Renderer* renderer);
-// Завершает кадр и показывает его на экране
 int renderer_end_frame(Renderer* renderer);
 
-// --- Функции отрисовки (теперь они просто добавляют данные в батч) ---
+// --- Функции отрисовки (добавляют данные в батч) ---
 void renderer_clear(Renderer* renderer, float r, float g, float b, float a);
 void renderer_draw_quad(Renderer* renderer, float x, float y, float width, float height, uint32_t color);
-void renderer_draw_text(Renderer* renderer, const char* text, float x, float y, float scale, uint32_t color);
+void renderer_draw_textured_quad(Renderer* renderer, float x, float y, float w, float h, float u0, float v0, float u1, float v1, uint32_t color);
+// --- ИЗМЕНЕНИЕ: Добавлен параметр max_width для обрезки текста ---
+void renderer_draw_text(Renderer* renderer, const char* text, float x, float y, float scale, uint32_t color, float max_width);
 
 // --- Геттеры ---
 int renderer_get_width(const Renderer* renderer);
 int renderer_get_height(const Renderer* renderer);
-// Получение ID текстуры атласа шрифта (нужно для text_renderer)
 GLuint renderer_get_font_atlas_texture(const Renderer* renderer);
 
 #endif // SEE_CODE_RENDERER_H
