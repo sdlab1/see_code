a4fe52257417a0d3956dbbf911cff8b1f11e41b2
sdlab1 <59804621+sdlab1@users.noreply.github.com>
2025-09-06 06:35:24 +0300

    Update renderer.h

--- src/gui/renderer.h
+++ src/gui/renderer.h
@@ -1,10 +1,9 @@
-// src/gui/renderer.h
+// src/gui/renderer.h - Fixed function signatures
 #ifndef SEE_CODE_RENDERER_H
 #define SEE_CODE_RENDERER_H
 
 #include <EGL/egl.h>
 #include <GLES2/gl2.h>
-// Remove freetype includes from here, they are now in text_renderer.h
 
 // Forward declarations
 typedef struct Renderer Renderer;
@@ -16,14 +15,16 @@ int renderer_begin_frame(Renderer* renderer);
 int renderer_end_frame(Renderer* renderer);
 void renderer_resize(Renderer* renderer, int width, int height);
 
-// Rendering functions
-void renderer_clear(float r, float g, float b, float a);
-void renderer_draw_quad(float x, float y, float width, float height,
+// Rendering functions - FIXED: Add Renderer parameter
+void renderer_clear(Renderer* renderer, float r, float g, float b, float a);
+void renderer_draw_quad(Renderer* renderer, float x, float y, float width, float height,
                        float r, float g, float b, float a);
 
-// --- ОБНОВЛЕНИЕ: Объявление новой функции ---
-// Объявляем основную функцию рендеринга текста, реализация в text_renderer.c
+// Text rendering
 void renderer_draw_text(Renderer* renderer, const char* text, float x, float y, float scale, unsigned int color);
-// --- КОНЕЦ ОБНОВЛЕНИЯ ---
+
+// Getters
+int renderer_get_width(const Renderer* renderer);
+int renderer_get_height(const Renderer* renderer);
 
 #endif // SEE_CODE_RENDERER_H
